3e8bf9e8da8e4c7dcebb9598866864df5452c680
Dan Kreibich <dan@lh-innovationhub.com>
2020-01-25 16:40:46 +0100

    remove default validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :instructor, :class_name => 'User'
   belongs_to :student, :class_name => 'User'
 
-  # validates :description, presence: true, length: { minimum: 50 }
-  # validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :description, presence: true, length: { minimum: 50 }
+  validates :rating, presence: true, inclusion: { in: 1..5 }
 end
