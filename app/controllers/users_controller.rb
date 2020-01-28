bc2415c828b5542fa99e765b1c314a9c94e83f90
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-28 16:36:47 +0100

    Added styles for nav bar. Change back the router for index and show page becouse of the routes error.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    # @instructor = User.find(params[:id])
+    @instructor = User.first
+    #@instructor = User.find(params[:id])
   end
 end
