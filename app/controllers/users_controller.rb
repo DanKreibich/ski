a570d7ef24709862d79236270b2d0ee88686da95
Dan Kreibich <dan@lh-innovationhub.com>
2020-02-01 14:42:26 +0100

    finished user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,6 @@ class UsersController < ApplicationController
   def show
     @instructor = User.find(params[:id])
     @reviews = Review.where(instructor_id: @instructor.id)
+    @photos = Photo.where(user_id: @instructor.id)
   end
 end
