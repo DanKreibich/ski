b70c2d9acf32fe3cadc355efb86084706087c31d
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-02-01 17:21:31 +0100

    booking table cells are now select / unselectable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+// import { myFunction } from '../trip_new.js.erb'
+
+const bookingSlots =  document.querySelectorAll(".booking-slot-open");
+
+bookingSlots.forEach((element) => {
+ element.addEventListener("click", () =>
+    element.classList.toggle("booked"));
+    checkbox.checked = !checkbox.checked
+ // To add toggle checkbox
+});
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Ski
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,7 +17,6 @@ module Ski
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
