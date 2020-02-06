52528186eabd338847ec564b5c133949dcc26845
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-02-06 20:07:46 +0100

    Fixed autocomplete option for form search

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,3 +1,4 @@
+// https://medium.com/lightthefuse/adding-autocomplete-suggestions-to-an-input-field-in-a-rails-application-with-webpack-b2a840a65087
 import 'js-autocomplete/auto-complete.css';
 import autocomplete from 'js-autocomplete';
 
@@ -22,30 +23,4 @@ const autocompleteSearch = function() {
   }
 };
 
-// export { autocompleteSearch };
-
-
-
-
-
-// const autocompleteSearch = function() {
-//   const skills = JSON.parse(document.getElementById('search-data').dataset.skills)
-//   const searchInput = document.getElementById('q');
-
-//   if (skills && searchInput) {
-//     new autocomplete({
-//       selector: searchInput,
-//       minChars: 1,
-//       source: function(term, suggest){
-//           term = term.toLowerCase();
-//           const choices = skills;
-//           const matches = [];
-//           for (let i = 0; i < choices.length; i++)
-//               if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
-//           suggest(matches);
-//       },
-//     });
-//   }
-// };
-
-// export { autocompleteSearch };
+export { autocompleteSearch };
