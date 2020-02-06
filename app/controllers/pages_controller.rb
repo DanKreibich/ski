c6bba9c283f3923365866f60a4ad801cfc5ed48f
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-02-06 15:03:24 +0100

    Made avatar and menu in one line (navbar on all pages)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :contact]
 
   def home
-    @resorts = User.pluck(:resort).uniq.sort
+    # @resorts = User.pluck(:resort).uniq.sort
   end
 
   def contact
