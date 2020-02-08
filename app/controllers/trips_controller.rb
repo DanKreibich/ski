fa88ab9d6abe648174450781c15465440387d0aa
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-02-08 13:02:09 +0100

    enabled trip and session saving

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,53 +11,52 @@ class TripsController < ApplicationController
 
   def create
 
-    raise
-    @session = Trip.new(trip_params)
-    # # temporary data
-    # today = Date.today
-    # session_one = "#{today + 1} 09:00:00 UTC +00:00".to_datetime
-    # session_two = "#{today + 2} 09:00:00 UTC +00:00".to_datetime
-    # session_three = "#{today + 3} 09:00:00 UTC +00:00".to_datetime
+    # the following list depicts what is expected to be received. Needs to be renamed ones real data comes
+    selected_sessions_array = []
+    params[:trip][:sessions].each do |key|
+      selected_sessions_array << key[0].to_datetime
+    end
+    # instructor_id should be passed in params
+    instructor_id = params[:user_id].to_i
+    # student_id should be passed in params
+    student_id = params[:trip][:student_id].to_i
+    # num_students should be passed in params
+    num_students = 2
+    # note should be passed in params
+    note = "Llorem ipsum Llorem ipsum Llorem ipsum Llorem ipsum"
+    # do a check if all sessions are still available
+    @instructor = User.find(instructor_id)
+    student = User.find(student_id)
+    @trips = Trip.where(instructor_id: instructor_id)
 
-    # # the following list depicts what is expected to be received. Needs to be renamed ones real data comes
-    # sessions_array = [session_one, session_two, session_three]
-    # # instructor_id should be passed in params
-    # instructor_id = User.first.id + 3
-    # # student_id should be passed in params
-    # student_id = instructor.id + 1
-    # # num_students should be passed in params
-    # num_students = 2
-    # # note should be passed in params
-    # note = "Llorem ipsum Llorem ipsum Llorem ipsum Llorem ipsum"
-    # # do a check if all sessions are still available
-    # @instructor = User.find(params[:user_id])
-    # student_id = instructor.id + 1 # !!!!!!!!!! Needs to be adjusted with live data
-    # @trips = Trip.where(instructor_id: instructor.id)
+    booked_sessions = [] # for all trips that have been already booked by other users
+    booked_sessions_starts = [] # are all the start times of booked_sessions
 
-    # booked_sessions = [] # for all trips that have been already booked by other users
-    # sessions_starts = [] # are all the start times of booked_sessions
+    @trips.each do |trip|
+      booked_sessions += Session.where(trip_id: trip.id).to_a
+      booked_sessions.each do |session|
+        booked_sessions_starts << session.start
+      end
+    end
 
-    # @trips.each do
-    #   booked_sessions += Session.where(trip_id: trip.id).to_a
-    #   booked_sessions.each do |session|
-    #     sessions_starts << session.start
-    #   end
-    # end
+    combined_sessions = []
+    combined_sessions += selected_sessions_array
+    combined_sessions += booked_sessions_starts # combines already booked sessions with requested sessions
+    if combined_sessions.uniq.length == combined_sessions.length # checks if there are overlaps
+      trip = Trip.new(instructor_id: instructor_id, student_id: student_id, num_students: num_students, note: note)
+      trip.save!
 
-    # sessions_array += sessions_starts # combines already booked sessions with requested sessions
+      selected_sessions_array.each do |start_datetime|
+        session = Session.new(trip_id: trip.id, start: start_datetime, end: start_datetime + 1.hour)
+        session.save!
+      end
+      redirect_to root_path
+    else
+      flash[:notice] = "Couldn't be saved as some slot had been booked by another user in the meantime. Please load page again."
+    end
 
-    # if sessions_array.uniq.length == sessions_array.length # checks if there are overlaps
-    #   trip = Trip.new(instructor_id: instructor_id, student_id: student_id, num_students: num_students, note: note)
-    #   trip.save!
-
-    #   sessions_array.each do |start_datetime|
-    #     session = Session.new(trip_id: trip.id, start: start_datetime, end: start_datetime + (1 / 24.0)) #last brackets add 1 hour to start_datetime
-    #     session.save!
-    #   end
-    # else
-    #   flash[:notice] = "Couldn't be saved as some slot had been booked by another user in the meantime. Please load page again."
-    # end
   end
+
 
   private
 
