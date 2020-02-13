e4f8235fea0b843a16f199807317253ee8224864
Dan Kreibich <dan@lh-innovationhub.com>
2020-02-13 17:10:43 +0100

    added money rails gem and funcionality

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'webpacker'
 # Added gems
 gem 'devise'
 gem 'faker'
+gem 'money-rails'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_135441) do
+ActiveRecord::Schema.define(version: 2020_02_13_155701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,13 +71,13 @@ ActiveRecord::Schema.define(version: 2020_02_08_135441) do
     t.text "description"
     t.integer "min_booking_hours"
     t.integer "level"
-    t.integer "hourly_rate"
     t.integer "day_rate"
     t.string "resort"
     t.string "avatar"
     t.integer "max_num_students"
     t.date "birth_date"
     t.integer "role"
+    t.integer "hourly_rate_cents", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
