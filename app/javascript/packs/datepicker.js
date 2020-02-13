6e5121a165c8b863059776f8f9511ab51516e000
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-02-13 19:47:46 +0100

    date picker validation done

--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -4,7 +4,10 @@ import "flatpickr/dist/flatpickr.min.css";
 //Best practice: done this way to avoid overwrting from other javascript files
 const datePickerHelper = {
   init: function() {
-    flatpickr(".datepicker", {allowInput: true});
+    flatpickr(".datepicker", {
+      allowInput: true,
+      minDate: new Date()
+    });
     const datefields = document.querySelectorAll(".datepicker");
     datefields.forEach(datefield => datefield.onpress = () => false);
   }
